a3395d0e8d0a6abb601c2bf24fc25557517b2557
NguyenBui96 <nguyenbui1996@gmail.com>
2017-07-17 17:12:43 -0700

    Re-init

--- app/src/main/java/com/sjsu/nguyen/alertsjsu/MapsActivity.java
+++ app/src/main/java/com/sjsu/nguyen/alertsjsu/MapsActivity.java
@@ -1,102 +1,29 @@
 package com.sjsu.nguyen.alertsjsu;
 
-import android.location.Location;
-import android.support.annotation.NonNull;
-import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
-import android.util.Log;
+import android.support.v4.app.FragmentActivity;
 
-import com.google.android.gms.common.ConnectionResult;
-import com.google.android.gms.common.api.GoogleApiClient;
-import com.google.android.gms.location.LocationServices;
-import com.google.android.gms.location.places.Places;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MapsActivity extends FragmentActivity
-        implements OnMapReadyCallback,
-        GoogleApiClient.ConnectionCallbacks,
-        GoogleApiClient.OnConnectionFailedListener {
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
-    private static final String TAG = MapsActivity.class.getSimpleName();
     private GoogleMap mMap;
-    private CameraPosition mCameraPosition;
-
-    // The entry point to Google Play services, used by the Places API and Fused Location Provider
-    private GoogleApiClient mGoogleApiClient;
-
-    // The geographical location where the device is currently located. That is, the last-known
-    // location retrieved by the Fused Location Provider
-    private Location mLastKnownLocation;
-
-    // Constants
-    private static final String KEY_LOCATION = "location";
-    private static final String KEY_CAMERA_POSITION = "camera_position";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        // Retrieve location and camera position from saved instance state
-        if(savedInstanceState != null){
-            mLastKnownLocation = savedInstanceState.getParcelable(KEY_LOCATION);
-            mCameraPosition = savedInstanceState.getParcelable(KEY_CAMERA_POSITION);
-        }
-
-        // Retrieve the content view that renders the map
         setContentView(R.layout.activity_maps);
-
-        // Build the Play services client for use by the Fused Location Provider and the Places API
-        // Use the addApi() method to request the Google Places API and the Fused Location Provider
-        mGoogleApiClient = new GoogleApiClient.Builder(this)
-                .enableAutoManage(this /* FragmentActivity */,
-                        this /* OnConnectionFailedListener */)
-                .addConnectionCallbacks(this)
-                .addApi(LocationServices.API)
-                .addApi(Places.GEO_DATA_API)
-                .addApi(Places.PLACE_DETECTION_API)
-                .build();
-        mGoogleApiClient.connect();
-
-    }
-
-    /**
-     * Builds the map when the Google Play services client is successfully connected
-     */
-
-    @Override
-    public void onConnected(Bundle connectionHint){
-        // Build the map
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
     }
 
-    /**
-     * Handles failure to connect to the Google Play services client
-     */
-
-    @Override
-    public void onConnectionFailed(@NonNull ConnectionResult result){
-        // Refer to the reference doc for ConnectionResult to see what error codes might
-        // be returned in onConnectionFailed
-        Log.d(TAG, "Play services connection failed: ConnectionResult.getErrorCode() = "
-                + result.getErrorCode());
-    }
-
-    /**
-     * Handles suspension of the connection to the Google Play services client
-     */
-
-    @Override
-    public void onConnectionSuspended(int cause){
-        Log.d(TAG, "Play services connection suspended");
-    }
 
     /**
      * Manipulates the map once available.
